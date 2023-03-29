22cda6499559c8e99d1fb893fab286a3fe768482
nvt <nvt@MacBook-Pro-nvt.local>
2023-03-29 03:32:17 +0300

    two

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var url = 'https://google.com';
 var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
 require('child_process').exec(start + ' ' + url);
+console.log('script end')
